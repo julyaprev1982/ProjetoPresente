db12b9e09afdc93f5eb420f696e65992b09ff47a
Juliana Revoredo <65569108+julyaprev1982@users.noreply.github.com>
2020-06-08 18:53:13 -0300

    Dados

---
+++ PresDB.cs
@@ -0,0 +1,37 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    public class PresDB
+    { //Testar
+        private static List<Pres> lstPres = new List<Pres>();
+
+        public PresDB()
+        {
+            //Implementar
+        }
+
+        public bool Salvar(Pres pres)
+        {
+            try
+            {
+                lstPres.Add(pres);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public List<Pres> ConsultarTudo()
+        {
+            return lstPres;
+        }
+    }
+}
